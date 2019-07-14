94a35430f8a8c3caf9eae443fd2b0e4b5340ac26
Dima <dr.trem86@gmail.com>
2019-07-14 16:35:10 +0300

    change front ADDR

--- frontend/src/service/ComponentsService.js
+++ frontend/src/service/ComponentsService.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const API_URL = '${http://${process.env.BACK_IP}:8080/api/components}';
+const API_URL = 'http://ADDR:8080/api/components';
 
 class ComponentsService {
     retrieveAllComponents() {
